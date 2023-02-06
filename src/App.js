07bafeaf00de4fa56cc2540668a3c4e36527471e
moonaim ghouali <ia_ghouali@esi.dz>
2023-02-06 13:01:14 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {TreeGridComponent} from "@syncfusion/ej2-react-treegrid"
+import data from "./data/dummyData"
 
 const App = () => {
   return (
-    <div>App</div>
+    <TreeGridComponent dataSource={data}>
+
+    </TreeGridComponent>
   )
 }
 
 export default App
+
---
+++ src/data/sideBarData.js
@@ -0,0 +1,21 @@
+export const links = [
+    {
+      name: 'Dashboard',
+      link : "/dashboard",
+      icon: "",
+    },
+    {
+      name: 'Production',
+      link : "/production",
+      icon: "",
+    },
+    {
+      name: 'KPI',
+      link : "/performance",
+      icon: "",
+    },
+    {
+      name: 'Reporting',
+      link : "/rapports",
+      icon: "",
+    }]
